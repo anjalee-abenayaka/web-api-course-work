12806baf6b0a785cb90c06e10ba3038ae8c6e410
anjalee-abenayaka <anjalee-abenayaka@users.noreply.github.com>
2020-03-11 23:53:38 +0530

    changed app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,17 +3,21 @@ import { Route, Switch} from "react-router-dom";
 import About from "./about";
 import Login from "./RegisterLogin";
 import Register from "./RegisterLogin/register";
+//import Footer from "./views/Footer/Footer"
+//import React, { Suspense } from 'react';
+//import NavBar from "./views/NavBar/NavBar";
 
 function App() {
   return (
+    
     <div>
       <Switch>
-        
         <Route path="/about" component={About}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
       </Switch>
     </div>
+  
   );
 }
 
