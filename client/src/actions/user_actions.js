e750b8f8bbf5853b098972f2e12cee5a37a9da22
anjalee-abenayaka <anjalee-abenayaka@users.noreply.github.com>
2020-03-11 22:14:24 +0530

    added few changes in registration

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
    
 } from './types';
 
@@ -8,8 +9,17 @@ export function loginUser(dataToSubmit){
     const request = axios.post('/api/users/login', dataToSubmit)
                          .then(response => response.data)
 
-                         return {
-                             type: LOGIN_USER,
-                             payload: request
-                         }
+ return {
+        type: LOGIN_USER,
+         payload: request
+   }
+}
+export function registerUser(dataToSubmit){
+    const request = axios.post('/api/users/register', dataToSubmit)
+                         .then(response => response.data)
+
+ return {
+        type: REGISTER_USER,
+         payload: request
+   }
 }
--- client/src/components/RegisterLogin/register.js
+++ client/src/components/RegisterLogin/register.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import  { connect } from 'react-redux'
+import { registerUser } from '../../actions/user_actions';
+//import { response } from 'express';
 
 export class Register extends Component {
 
@@ -11,8 +14,82 @@ export class Register extends Component {
         errors: []
     };
 
+    displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error}</p>)
+
     handleChange = event =>{
         this.setState({ [event.target.name]: event.target.value })
+    }
+
+    isFormValid = () =>{
+        let errors = [];
+        let error;
+        if(this.isFormEmpty(this.state)){
+            error = { message: "Fill in all fields"};
+            this.setState({ errors : errors.concat(error)});
+        }else if (!this.isPasswordValid(this.state)){
+            error = { message: "Password is invalid"};
+            this.setState({ errors: errors.concat(error)});
+        }else {
+            return true;
+        }
+
+    }
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if(password.length < 6 || passwordConfirmation.length < 6){
+            return false;
+        }else if (password !== passwordConfirmation){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    isFormEmpty = ({ name, lastname, email, password, passwordConfirmation }) =>{
+        return(
+            !name.length ||
+            !lastname.length ||
+            !email.length ||
+            !password.length ||
+            !passwordConfirmation.length 
+        );
+    }
+
+    submitForm = event => {
+        event.preventDefault();
+
+        let dataToSubmit = {
+            email:this.state.email,
+            name:this.state.name,
+            lastname:this.state.lastname,
+            password:this.state.password,
+            passwordConfirmation:this.state.passwordConfirmation
+            
+        }
+
+        if(this.isFormValid()){
+        this.setState({ errors: [] })
+        this.props.dispatch(registerUser(dataToSubmit))
+        .then(response => {
+      
+            if(response.payload.success){
+             this.props.history.push('/login')
+            }else{
+                this.setState({
+                    errors: this.state.errors.concat("Please check your details")
+                })
+
+           }
+        })
+        .catch(err => {
+            this.setState({
+                errors: this.state.errors.concat(err)
+            });
+        })
+        }else{
+            console.error("Form is not valid")
+        }
     }
     render() {
         return (
@@ -29,7 +106,7 @@ export class Register extends Component {
                                  type="text"
                                  className="validate"
                             />
-                            <label className="active" htmlFor="email">First Name</label>
+                            <label className="active" htmlFor="name">First Name</label>
                             <span
                                  className="helper-text"
                                  data-error="type a right email"
@@ -46,7 +123,7 @@ export class Register extends Component {
                                    type="text"
                                    className="validate"
                             />
-                            <label className="active" htmlFor="email">Last Name</label>
+                            <label className="active" htmlFor="lastname">Last Name</label>
                             <span
                                  className="helper-text"
                                   data-error="wrong"
@@ -80,7 +157,7 @@ export class Register extends Component {
                                    type="password"
                                    className="validate"
                         />
-                        <label className="active" htmlFor="email">Password</label>
+                        <label className="active" htmlFor="password">Password</label>
                         <span
                               className="helper-text"
                               data-error="wrong"
@@ -132,4 +209,4 @@ export class Register extends Component {
     }
 }
 
-export default Register
+export default connect()(Register);
