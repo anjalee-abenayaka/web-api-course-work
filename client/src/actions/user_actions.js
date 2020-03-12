8634480431a6c7895029f201c06456569482c73d
anjalee-abenayaka <anjalee-abenayaka@users.noreply.github.com>
2020-03-12 09:50:57 +0530

    added navigation bar and footer

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import {
     LOGIN_USER,
-    REGISTER_USER
+    REGISTER_USER,
+    AUTH_USER,
    
 } from './types';
 
 export function loginUser(dataToSubmit){
-    const request = axios.post('/api/users/login', dataToSubmit)
+    const request = axios.post(`/api/users/login`, dataToSubmit)
                          .then(response => response.data)
 
  return {
@@ -15,11 +16,22 @@ export function loginUser(dataToSubmit){
    }
 }
 export function registerUser(dataToSubmit){
-    const request = axios.post('/api/users/register', dataToSubmit)
+    const request = axios.post(`/api/users/register`, dataToSubmit)
                          .then(response => response.data)
 
  return {
         type: REGISTER_USER,
          payload: request
    }
+}
+
+
+export function auth() {
+    const request = axios.get(`/api/users/auth`)
+        .then(response => response.data);
+
+    return {
+        type: AUTH_USER,
+        payload: request
+    }
 }
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import { Route, Switch} from "react-router-dom";
+
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
+// pages for this product
 import About from "./about";
 import Login from "./RegisterLogin";
 import Register from "./RegisterLogin/register";
 //import Footer from "./views/Footer/Footer"
 //import React, { Suspense } from 'react';
 //import NavBar from "./views/NavBar/NavBar";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer"
 
 function App() {
   return (
     
-    <div>
-      <Switch>
-        <Route path="/about" component={About}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/register" component={Register}/>
-      </Switch>
-    </div>
+     <Suspense fallback={(<div>Loading...</div>)}>
+     <NavBar />
+     <div style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
+       <Switch>
+         <Route exact path="/login" component={Auth(Login, false)} />
+         <Route exact path="/register" component={Auth(Register, false)} />
+       </Switch>
+     </div>
+     <Footer />
+   </Suspense>
   
   );
 }
