e45f3e651eb77618eab3a8c61aab9a7af4c4399f
YanikPerera <48877894+YanikPerera@users.noreply.github.com>
2020-03-22 22:17:47 +0530

    post method tested user loggin

--- server/index.js
+++ server/index.js
@@ -49,34 +49,8 @@ app.post('/api/users/register', (req, res)=>
     });
 
 });
-app.post('/api/users/login', (req, res) => {
-    //find the email
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if(!user)
-        return res.json({
-            loginSuccess: false,
-            message: "Auth failed, email not found"
-        });
 
-        //compare password
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if(!isMatch){
-                return res.json ({ loginSuccess: false, message: "wrong password"});
-            }
-        });
-
-        //generate token
-        user.generateToken((err, user) =>{
-            if(err) return res.status(400).send(err);
-            res.cookie("x_auth", user.token)
-               .status(200)
-               .json({
-                   loginSuccess: true
-               });
-        });
-    });
-});
-
+// test post user post method
 app.get("/api/users/logout", auth, (req,res) =>{
     User.findByIdAndUpdate({_id: req.user._id}, { token: ""}, (err, doc)=>{
         if(err) return res.json({ success: false, err })
